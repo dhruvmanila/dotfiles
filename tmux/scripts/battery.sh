2bd72d401f7bdf8677574a071c542b1a70bd26ba
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-01-28 18:35:01 +0530

    Updated battery.sh for tmux statusline

--- tmux/scripts/battery.sh
+++ tmux/scripts/battery.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-pmset -g batt | grep -o "\d\d%"
+CHARGING_SYMBOL="⚡"
+
+battery_status=$(pmset -g batt)
+percentage=$(echo "$battery_status" | grep -Eo '[0-9]{1,3}%')
+
+if echo "$battery_status" | grep 'charging' &>/dev/null; then
+  printf "%s%s" "${CHARGING_SYMBOL}" "${percentage}"
+else
+  printf "%s" "${percentage}"
+fi
