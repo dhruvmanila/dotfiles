e5b30713e4179d16a68c60d154310ddb8f3ace87
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-02-16 21:09:12 +0530

    Update vim and tmux config
    - Update coc config to have two options:
      1. Ability to only start coc manually
      2. Ability to only start coc on certain filetypes
    - Add ability to change tmux statusline colors along with the vim
      colorscheme and vim statusline colors (lightline)
    - Use normal separators instead of powerline separators
    - Add function to change iTerm2 profile (does not work in tmux)
    - Make plugin config in vimrc alphabetical. Call the function which
      changes the colorscheme at the end of vimrc and do not call subsequent
      reload

--- tmux/scripts/battery.sh
+++ tmux/scripts/battery.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
 battery_status=$(pmset -g batt)
-percentage=$(echo "$battery_status" | grep -Eo '[0-9]{1,3}%' | cut -c 1-2)
+percentage=$(echo "$battery_status" | grep -Eo '[0-9]{1,3}%')
 
-if [[ $percentage -lt 20 ]]; then
-  tmux_fg=red
-  tmux_attr=,bold
-elif [[ $percentage -lt 80 ]]; then
-  tmux_fg=yellow
-  tmux_attr=
-else
-  tmux_fg=green
-  tmux_attr=
-fi
+# if [[ $percentage -lt 20 ]]; then
+#   tmux_fg=red
+#   tmux_attr=,bold
+# elif [[ $percentage -lt 80 ]]; then
+#   tmux_fg=yellow
+#   tmux_attr=
+# else
+#   tmux_fg=green
+#   tmux_attr=
+# fi
 
 if echo "$battery_status" | grep -i 'ac power' &>/dev/null; then
-  charging=⚡
+  charging="⚡"
 else
-  charging=
+  charging=""
 fi
 
-printf "#[fg=%s%s]%s%s%%" "$tmux_fg" "$tmux_attr" "$charging" "$percentage"
+# printf "#[fg=%s%s]%s%s%%" "$tmux_fg" "$tmux_attr" "$charging" "$percentage"
+printf "%s%s" "$charging" "$percentage"
