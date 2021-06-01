f5341f9985df37963a605bcbf29762e12053bf6a
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-06-01 18:45:19 +0530

    chore: use block for auto installing packer
    Also, require packer after the auto installation step otherwise when
    Neovim is opened for the first time, it will error out.

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -1,13 +1,17 @@
-local packer = require("packer")
-local cmd = vim.api.nvim_command
 local fn = vim.fn
+local cmd = vim.api.nvim_command
 
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+do
+  local install_path = fn.stdpath("data")
+    .. "/site/pack/packer/start/packer.nvim"
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-  cmd("packadd packer.nvim")
+  if fn.empty(fn.glob(install_path)) > 0 then
+    cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    cmd("packadd packer.nvim")
+  end
 end
+
+local packer = require("packer")
 
 -- PackerSync -> PackerUpdate + PackerClean + PackerCompile
 vim.api.nvim_set_keymap(
