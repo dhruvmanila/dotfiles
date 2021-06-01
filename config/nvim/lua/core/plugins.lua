9ea1d6f4c9e2f87be7d0fbb5e1797f8e13b9f751
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-06-01 18:46:20 +0530

    feat(plugin): trying out clever-f

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -198,6 +198,18 @@ packer.startup({
     use("tpope/vim-scriptease")
     use("tpope/vim-eunuch")
 
+    -- Motion
+    use({
+      "rhysd/clever-f.vim",
+      setup = function()
+        vim.g.clever_f_across_no_line = 1
+        vim.g.clever_f_smart_case = 1
+        vim.g.clever_f_show_prompt = 1
+        -- `f;` and `f:` matches all signs
+        vim.g.clever_f_chars_match_any_signs = ";:"
+      end,
+    })
+
     -- Pretification
     use({
       "junegunn/vim-easy-align",
