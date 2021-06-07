b3dba5452af2e40e156d7baa9a47037283f2394b
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-06-07 14:36:14 +0530

    refactor(format): add '-sr' flag to shfmt
    '-sr': redirect operators will be followed by a space
    
    Example:
      `echo 'hello world' > test`  (with -sr)
      `echo 'hello world' >test`   (without -sr)

--- config/nvim/lua/core/format/init.lua
+++ config/nvim/lua/core/format/init.lua
@@ -52,12 +52,13 @@ format.formatter("sh", {
   -- -i uint   indent: 0 for tabs (default), >0 for number of spaces
   -- -bn       binary ops like && and | may start a line
   -- -ci       switch cases will be indented
+  -- -sr       redirect operators will be followed by a space
   -- -kp       keep column alignment paddings
   args = function(bufnr)
     local indent_size = vim.bo[bufnr].expandtab
         and lsp_util.get_effective_tabstop(bufnr)
       or 0
-    return { "-i", indent_size, "-bn", "-ci", "-kp", "-" }
+    return { "-i", indent_size, "-bn", "-ci", "-sr", "-kp", "-" }
   end,
   stdin = true,
 })
