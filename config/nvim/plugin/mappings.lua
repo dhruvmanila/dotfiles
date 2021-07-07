4142108c7763f6b794cd5f6ca5bf56e360137d2d
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-07-07 23:20:04 +0530

    refactor: add/update mappings and description
    Mappings regarding save and quit will use 'update' which will save the
    buffer only if it is changed.
    
    For line movements, '^' and '0' will move on physical/visual lines, not
    the actual lines which could be wrapped.
    
    Use 'g_' to jump to end of the line as this will jump to non-blank
    character of the line.
    
    On yanking (visual) and pasting (normal + visual), jump to the end of
    the yanked or pasted text. It was annoying sometimes when I would select
    a few lines and then yank it and the cursor would go right at the
    beginning.
    
    Remove split with leader bindings as I directly use the `<C-w>*`
    bindings and as such I don't do horizontal splits that much.
    
    Bind 'c' and 'C' similar to 'x' and 'X' which would override what was in
    the clipboard.
    
    Change the case for the word under cursor to upper/title case in insert
    mode.

--- config/nvim/plugin/mappings.lua
+++ config/nvim/plugin/mappings.lua
@@ -3,27 +3,43 @@ local map = require("dm.utils").map
 -- By using <Cmd> instead of ':', the CmdlineEnter and other related events
 -- will not be triggered and thus, no need for <silent>.
 
--- 'Q' in normal mode enters Ex mode. You almost never want this.
-map("n", "Q", "<Cmd>qa<CR>")
+-- Shortcuts for faster save and quit
+--
+-- '<leader>w': Save only when buffer is updated
+-- '<leader>q': Save the file if modified and quit
+-- 'Q': Save all changed buffers and exit vim
+--
+-- The reason for using <silent> instead of <Cmd> is to trigger Cmdline*
+-- events so that the "write message" can be cleared.
+map("n", "<Leader>w", ":update<CR>", { silent = true })
+map("n", "<Leader>q", ":x<CR>", { silent = true })
+map("n", "Q", "<Cmd>xall<CR>")
 
--- Quick save
-map("n", "<Leader>w", ":w<CR>", { silent = true })
-map("n", "<Leader>q", ":q<CR>", { silent = true })
--- map('n', '<Leader>wq', ':wq<CR>')
-
--- Make 'Y' behave same as 'C', 'S'
+-- Yank from current cursor position to the end of the line. Make it consistent
+-- with the behavior of 'C', 'D'
 map("n", "Y", "y$")
 
--- Move vertically by visual line
-map("n", "j", "gj")
-map("n", "k", "gk")
+-- Move the cursor based on physical lines, not the actual lines.
+map("n", "j", [[ (v:count == 0 ? 'gj' : 'j') ]], { expr = true })
+map("n", "k", [[ (v:count == 0 ? 'gk' : 'k') ]], { expr = true })
+map("n", "^", "g^")
+map("n", "0", "g0")
+map("x", "j", "gj")
+map("x", "k", "gk")
+
+-- Jump to start and end of line using the home row keys
+map({ "n", "x" }, "H", "^")
+map({ "n", "x" }, "L", "g_")
+
+-- Automatically jump to the end of text on yank and paste
+-- ` (backticks) to the last character of previously changed or yanked text
+map("n", "p", "p`]")
+map("x", "p", "p`]")
+map("x", "y", "y`]")
 
 -- Easy command mode if not using registers explicitly
 -- map('n', "'", ';')
 -- map('n', ';', ':')
-
--- Split with Leader
-map("n", "<Leader>-", "<Cmd>sp<CR>")
 
 -- Buffer management
 map("n", "]<Leader>", "<Cmd>bnext<CR>")
@@ -82,16 +98,14 @@ map("n", "<Leader>su", [[:%s/\<<C-r><C-w>\>//g<Left><Left>]])
 -- Source files (works only with either lua or vim files)
 map("n", "<leader>so", ":source %<CR>", { silent = true })
 
--- Jump to start and end of line using the home row keys
-map({ "n", "x" }, "H", "^")
-map({ "n", "x" }, "L", "$")
-
 -- Delete without overriding the paste register
 map({ "n", "x" }, "<Leader>d", '"_d')
 
--- Prevent x from overriding what's in the clipboard
+-- Prevent 'x/X' and 'c/C' from overriding what's in the clipboard
 map({ "n", "x" }, "x", '"_x')
 map({ "n", "x" }, "X", '"_X')
+map({ "n", "x" }, "c", '"_c')
+map({ "n", "x" }, "C", '"_C')
 
 -- Replace the selection without overriding the paste register
 map("x", "p", '"_dP')
@@ -114,6 +128,12 @@ map("c", "<C-j>", "<Down>")
 -- map('i', '<C-l>', '<C-o>l')
 -- map('i', '<C-j>', '<C-o>j')
 -- map('i', '<C-k>', '<C-o>k')
+
+-- Change the case for the word under cursor in insert mode
+-- '<C-u>': (u)ppercase
+-- '<C-t>': (t)itlecase
+map("i", "<C-u>", "<esc>viwUea")
+map("i", "<C-t>", "<esc>b~lea")
 
 -- Normal mode in terminal
 map("t", "<Esc>", [[<C-\><C-n>]])
