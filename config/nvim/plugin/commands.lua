b9c1b0f31b00964e77cc47d45830c97dbeff4e05
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-07-12 08:44:03 +0530

    docs: custom commands documentation

--- config/nvim/plugin/commands.lua
+++ config/nvim/plugin/commands.lua
@@ -1,42 +1,44 @@
-local api = vim.api
 local fn = vim.fn
+local api = vim.api
 
-do
-  -- Trim trailing whitespace for the current buffer, restoring the
-  -- cursor position.
-  local function trim_trailing_whitespace()
-    local pos = api.nvim_win_get_cursor(0)
-    vim.cmd [[keeppatterns keepjumps %s/\s\+$//e]]
-    api.nvim_win_set_cursor(0, pos)
+-- :TrimTrailingWhitespace - Trim trailing whitespace for the current buffer,
+-- restoring the cursor position.
+--
+-- This command can be followed by a "|" and another command.
+dm.command { "TrimTrailingWhitespace", function()
+  local pos = api.nvim_win_get_cursor(0)
+  vim.cmd [[keeppatterns keepjumps %s/\s\+$//e]]
+  api.nvim_win_set_cursor(0, pos)
+end, attr = {
+  "-bar",
+} }
+
+-- :TrimTrailingLines - Trim blank lines at the end of the current buffer,
+-- restoring the cursor position.
+--
+-- This command can be followed by a "|" and another command.
+dm.command { "TrimTrailingLines", function()
+  local pos = api.nvim_win_get_cursor(0)
+  local last_line = api.nvim_buf_line_count(0)
+  local last_non_blank_line = fn.prevnonblank(last_line)
+
+  if last_non_blank_line > 0 and last_line ~= last_non_blank_line then
+    api.nvim_buf_set_lines(0, last_non_blank_line, last_line, false, {})
   end
 
-  dm.command {
-    "TrimTrailingWhitespace",
-    trim_trailing_whitespace,
-    attr = { "-bar" },
-  }
-end
+  api.nvim_win_set_cursor(0, pos)
+end, attr = {
+  "-bar",
+} }
 
-do
-  -- Trim blank lines at the end of the current buffer, restoring the
-  -- cursor position.
-  local function trim_trailing_lines()
-    local pos = api.nvim_win_get_cursor(0)
-    local last_line = api.nvim_buf_line_count(0)
-    local last_non_blank_line = fn.prevnonblank(last_line)
-
-    if last_non_blank_line > 0 and last_line ~= last_non_blank_line then
-      api.nvim_buf_set_lines(0, last_non_blank_line, last_line, false, {})
-    end
-
-    api.nvim_win_set_cursor(0, pos)
-  end
-
-  dm.command { "TrimTrailingLines", trim_trailing_lines, attr = { "-bar" } }
-end
-
+-- :Term - Open the terminal on the bottom occupying full width of the editor.
 dm.command { "Term", "new | wincmd J | resize -5 | term" }
+
+-- :Vterm - Open the terminal on the right hand side occupying full height of
+-- the editor.
 dm.command { "Vterm", "vnew | wincmd L | term" }
+
+-- :Tterm - Open the terminal in a new tab.
 dm.command { "Tterm", "tabnew | term" }
 
 -- :BufOnly - Delete all the buffers except the current buffer while ignoring
