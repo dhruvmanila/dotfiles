05f77ba23fca1499dd31e3d116f066de7c15ecb3
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-07-15 19:34:49 +0530

    feat: playing around with `luasnip`
    I got to say that this is quite powerful and we can get as complex as we
    want with all the different types of nodes.
    
    I am thinking about organizing the snippets like so:
    
    dm/
    ├── snippets
    │   ├── all.lua
    │   ├── lua.lua
    │   ├── python.lua
    │   ├── ...
    │   ├── utils.lua      # Utility functions used for defining snippets
    │   └── init.lua       # Initialization and setup LuaSnip
    └── ...
    
    `init.lua` will also `dofile` all the language snippets.

--- config/nvim/lua/dm/plugin/luasnip.lua
+++ config/nvim/lua/dm/plugin/luasnip.lua
@@ -1,6 +1,93 @@
-local luasnip = require "luasnip"
+local ls = require "luasnip"
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local c = ls.choice_node
+local f = ls.function_node
+local d = ls.dynamic_node
 
-luasnip.config.set_config {
+-- If only one argument is passed, then return that many number of tabs,
+-- otherwise return 'size' number of tabs (optional) prefixed to 'text'.
+---@param text string
+---@param size? number
+---@return string
+---@overload fun(size: number): string
+local function indent(text, size)
+  size = size or 1
+  if type(text) == "number" then
+    size = text
+    text = ""
+  end
+  return string.rep("\t", size) .. text
+end
+
+ls.config.set_config {
   history = true,
   updateevents = "TextChanged,TextChangedI",
+}
+
+ls.snippets.lua = {
+  -- string.format("$1", $2)$0
+  s(
+    { trig = "format", dscr = "Format string" },
+    { t 'string.format("', i(1, "formatstring"), t '", ', i(2), t ")", i(0) }
+  ),
+
+  -- require("$1")$0
+  s(
+    { trig = "req", dscr = "require a lua module" },
+    { t 'require("', i(1, "modname: string"), t '")' }
+  ),
+
+  -- A component to be used with 'autocmd' and 'augroup' snippets.
+  -- {
+  --   group = $1<string>,               TODO
+  --   events = $2<string|string[]>,
+  --   targets = $3<string|string[]>,
+  --   modifiers = $4<string|string[]>,  TODO
+  --   command = $5<string|function>,
+  -- }$0
+  s({ trig = "au", dscr = "autocmd/augroup component" }, {
+    t { "{", indent "events = " },
+    c(1, {
+      sn(nil, { t '"', i(1, "event: string"), t '"' }),
+      sn(nil, { t "{", i(1, "events: string[]"), t "}" }),
+    }),
+    t { ",", indent "targets = " },
+    c(2, {
+      sn(nil, { t '"', i(1, "target: string"), t '"' }),
+      sn(nil, { t "{", i(1, "targets: string[]"), t "}" }),
+    }),
+    t { ",", indent "command = " },
+    c(3, {
+      sn(nil, { t '"', i(1, "command: string"), t '",' }),
+      sn(nil, { i(1, "name: function"), t "," }),
+      sn(nil, {
+        t { "function()", indent(2) },
+        i(1, "body"),
+        t { "", indent "end," },
+      }),
+    }),
+    t { "", "}" },
+    i(0),
+  }),
+
+  -- dm.autocmd($0)
+  s({ trig = "autocmd", dscr = "Define autocmd using native function" }, {
+    t "dm.autocmd ",
+    i(0),
+  }),
+
+  -- dm.augroup($1<string>, {
+  --   $2
+  -- }$0
+  s({ trig = "augroup", dscr = "Define augroup using native function" }, {
+    t 'dm.augroup("',
+    i(1, "name: string"),
+    t { '", {', indent(1) },
+    i(2),
+    t { "", "})" },
+    i(0),
+  }),
 }
