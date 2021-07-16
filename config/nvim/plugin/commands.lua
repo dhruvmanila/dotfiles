253e3e62f1dd9b63c463a46c8adc01d61b0d8afe
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-07-16 10:11:38 +0530

    style: format commands in separate lines

--- config/nvim/plugin/commands.lua
+++ config/nvim/plugin/commands.lua
@@ -6,31 +6,39 @@ local command = dm.command
 -- restoring the cursor position.
 --
 -- This command can be followed by a "|" and another command.
-command { "TrimTrailingWhitespace", function()
-  local pos = api.nvim_win_get_cursor(0)
-  vim.cmd [[keeppatterns keepjumps %s/\s\+$//e]]
-  api.nvim_win_set_cursor(0, pos)
-end, attr = {
-  "-bar",
-} }
+command {
+  "TrimTrailingWhitespace",
+  function()
+    local pos = api.nvim_win_get_cursor(0)
+    vim.cmd [[keeppatterns keepjumps %s/\s\+$//e]]
+    api.nvim_win_set_cursor(0, pos)
+  end,
+  attr = {
+    "-bar",
+  },
+}
 
 -- :TrimTrailingLines - Trim blank lines at the end of the current buffer,
 -- restoring the cursor position.
 --
 -- This command can be followed by a "|" and another command.
-command { "TrimTrailingLines", function()
-  local pos = api.nvim_win_get_cursor(0)
-  local last_line = api.nvim_buf_line_count(0)
-  local last_non_blank_line = fn.prevnonblank(last_line)
+command {
+  "TrimTrailingLines",
+  function()
+    local pos = api.nvim_win_get_cursor(0)
+    local last_line = api.nvim_buf_line_count(0)
+    local last_non_blank_line = fn.prevnonblank(last_line)
 
-  if last_non_blank_line > 0 and last_line ~= last_non_blank_line then
-    api.nvim_buf_set_lines(0, last_non_blank_line, last_line, false, {})
-  end
+    if last_non_blank_line > 0 and last_line ~= last_non_blank_line then
+      api.nvim_buf_set_lines(0, last_non_blank_line, last_line, false, {})
+    end
 
-  api.nvim_win_set_cursor(0, pos)
-end, attr = {
-  "-bar",
-} }
+    api.nvim_win_set_cursor(0, pos)
+  end,
+  attr = {
+    "-bar",
+  },
+}
 
 -- :Term - Open the terminal on the bottom occupying full width of the editor.
 command { "Term", "new | wincmd J | resize -5 | term" }
@@ -44,38 +52,47 @@ command { "Tterm", "tabnew | term" }
 
 -- :BufOnly - Delete all the buffers except the current buffer while ignoring
 -- any terminal buffers.
-command { "BufOnly", function()
-  local deleted, modified = 0, 0
-  local curr_buf = api.nvim_get_current_buf()
-  for _, bufnr in ipairs(api.nvim_list_bufs()) do
-    if vim.bo[bufnr].buflisted then
-      if vim.bo[bufnr].modified then
-        modified = modified + 1
-      elseif bufnr ~= curr_buf and vim.bo[bufnr].buftype ~= "terminal" then
-        api.nvim_buf_delete(bufnr, {})
-        deleted = deleted + 1
+command {
+  "BufOnly",
+  function()
+    local deleted, modified = 0, 0
+    local curr_buf = api.nvim_get_current_buf()
+    for _, bufnr in ipairs(api.nvim_list_bufs()) do
+      if vim.bo[bufnr].buflisted then
+        if vim.bo[bufnr].modified then
+          modified = modified + 1
+        elseif bufnr ~= curr_buf and vim.bo[bufnr].buftype ~= "terminal" then
+          api.nvim_buf_delete(bufnr, {})
+          deleted = deleted + 1
+        end
       end
     end
-  end
-  if deleted > 0 or modified > 0 then
-    vim.notify {
-      "BufOnly",
-      "",
-      deleted .. " deleted buffer(s)",
-      modified .. " modified buffer(s)",
-    }
-  end
-end }
+    if deleted > 0 or modified > 0 then
+      vim.notify {
+        "BufOnly",
+        "",
+        deleted .. " deleted buffer(s)",
+        modified .. " modified buffer(s)",
+      }
+    end
+  end,
+}
 
 -- :LspLog - Open LSP logs for the builtin client in the bottom part of the
 -- window occupying full width.
-command { "LspLog", function()
-  vim.cmd "botright split"
-  vim.cmd "resize 20"
-  vim.cmd("edit + " .. vim.lsp.get_log_path())
-end }
+command {
+  "LspLog",
+  function()
+    vim.cmd "botright split"
+    vim.cmd "resize 20"
+    vim.cmd("edit + " .. vim.lsp.get_log_path())
+  end,
+}
 
 -- :LspClients - Print out all the LSP client information.
-command { "LspClients", function()
-  print(vim.inspect(vim.lsp.buf_get_clients()))
-end }
+command {
+  "LspClients",
+  function()
+    print(vim.inspect(vim.lsp.buf_get_clients()))
+  end,
+}
