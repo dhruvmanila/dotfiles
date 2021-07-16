3e0245d082e617ef72a8cd23dc5b6026636f6704
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-07-16 10:12:54 +0530

    feat: `q` to quit LspInfo window

--- config/nvim/after/ftplugin/lspinfo.lua
+++ config/nvim/after/ftplugin/lspinfo.lua
@@ -2,3 +2,5 @@ vim.api.nvim_win_set_config(
   vim.api.nvim_get_current_win(),
   { border = dm.border[vim.g.border_style] }
 )
+
+dm.nnoremap { "q", "<Cmd>bdelete<CR>", nowait = true, buffer = true }
