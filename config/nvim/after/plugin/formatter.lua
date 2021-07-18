edc40adb849018642c417fc67a2f6f8ad2dfb1e7
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-07-18 23:39:36 +0530

    refactor: move dashboard & formatter setup to lua

---
+++ config/nvim/after/plugin/formatter.lua
@@ -0,0 +1,29 @@
+-- Custom formatting setup using libUV
+-- TODO: setup auto formatting per buffer using a buffer variable
+
+local format = require("dm.formatter.format").format
+
+-- Flag to denote the current state of auto formatting.
+local auto_formatting = false
+
+-- Toggle between the two states of auto formatting.
+local function toggle_auto_formatting()
+  local commands = {}
+  auto_formatting = not auto_formatting
+  if auto_formatting then
+    table.insert(commands, {
+      events = "BufWritePost",
+      targets = "*",
+      command = format,
+    })
+  end
+  dm.augroup("dm__auto_formatting", commands)
+end
+
+dm.command { "ToggleAutoFormatting", toggle_auto_formatting }
+dm.command { "Format", format }
+
+dm.nnoremap { ";f", "<Cmd>Format<CR>" }
+
+-- By default, auto formatting is ON.
+toggle_auto_formatting()
