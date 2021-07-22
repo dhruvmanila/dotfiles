23066752412136550a2549d31c6de9e4e65757f4
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-07-22 12:58:21 +0530

    feat(fugitive): vertical integration
    So, I couldn't help it and found a way to integrate fugitive to a
    vertical split.
    
    My previous attempts (3f884ab, 01abde2, ec56054, caf6b47, e62e547) had
    bad heuristics as I was using number of windows in the current tabpage.
    
    This time I am using the exact layout of the windows in the tabpage and
    other heuristics to determine whether the fugitive window needs to be
    relocated in a vertical space.

--- config/nvim/after/ftplugin/fugitive.lua
+++ config/nvim/after/ftplugin/fugitive.lua
@@ -1,11 +1,69 @@
+local api = vim.api
 local opt_local = vim.opt_local
 local nmap = dm.nmap
+local nnoremap = dm.nnoremap
 
 opt_local.number = false
 opt_local.relativenumber = false
 opt_local.list = false
 
+-- Return the filetype for the buffer contained in the given window.
+---@param winnr number
+---@return string
+local function getwinft(winnr)
+  return vim.bo[api.nvim_win_get_buf(winnr)].filetype
+end
+
+-- Determine whether we have enough vertical space to move the fugitive buffer
+-- in a vertical position.
+--
+-- Heuristics:
+--   - Check if there is enough space available.
+--   - Check if there are any vertical splits.
+--   - If there are vertical splits, then it should contain only two windows
+--     of which the first one should be the explorer.
+--
+-- When opening the fugitive buffer for the first time, it is opened at the
+-- bottom part of the editor with full width. This will be excluded from the
+-- layout so that we only consider the top half of the editor.
+--
+-- NOTE: This should be called only if we are not already in a vertical position.
+---@return boolean
+local function has_vertical_space()
+  if vim.o.columns <= 140 then
+    return false
+  end
+  local layout = vim.fn.winlayout()
+  layout = layout[2][1]
+  if layout[1] == "row" then
+    local vert_wins = layout[2]
+    return #vert_wins == 2 and getwinft(vert_wins[1][2]) == "NvimTree"
+  end
+  return true
+end
+
+-- Determine whether we are in a vertical fugitive window. This is determined
+-- by looking at the position of the window.
+---@return boolean
+local function is_vertical_fugitive()
+  local row, col = unpack(api.nvim_win_get_position(0))
+  return col > 0 and row == 1
+end
+
+local vertical_fugitive = is_vertical_fugitive()
+if not vertical_fugitive and has_vertical_space() then
+  vim.cmd "wincmd L"
+  vertical_fugitive = true
+end
+
 local opts = { buffer = true, nowait = true }
 
-nmap("gh", "g?", opts)
+-- Setup the keybindings to open the window in the correct split.
+if vertical_fugitive then
+  nmap("gh", "g?", opts)
+else
+  nnoremap("gh", "<Cmd>vertical help fugitive-map<CR>", opts)
+  nnoremap("cc", "<Cmd>vertical Git commit<CR>", opts)
+end
+
 nmap("q", "gq", opts)
