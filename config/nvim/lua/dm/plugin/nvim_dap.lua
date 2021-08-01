adeb80906e187573102abb4db140072bbec4e15c
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-08-01 12:23:43 +0530

    refactor(dapui): remove default config options
    Only keep the config options which is overridden.

--- config/nvim/lua/dm/plugin/nvim_dap.lua
+++ config/nvim/lua/dm/plugin/nvim_dap.lua
@@ -62,38 +62,15 @@ do
 end
 
 require("dapui").setup {
-  icons = {
-    expanded = "▾",
-    collapsed = "▸",
-  },
   mappings = {
-    -- Use a table to apply multiple mappings
     expand = { "<CR>", "<2-LeftMouse>", "<Space>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
   },
   sidebar = {
     open_on_start = true,
     elements = {
-      "scopes",
-      "breakpoints",
-      "stacks",
-      -- "watches",
+      { id = "scopes", size = 0.6 },
+      { id = "breakpoints", size = 0.2 },
+      { id = "stacks", size = 0.2 },
     },
-    width = 40,
-    position = "left", -- Can be "left" or "right"
-  },
-  tray = {
-    open_on_start = true,
-    elements = {
-      "repl",
-    },
-    height = 10,
-    position = "bottom", -- Can be "bottom" or "top"
-  },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
   },
 }
