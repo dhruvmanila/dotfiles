bd75e279884d9afa904537d3e2fc242e77e38385
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-08-12 17:31:57 +0530

    feat(neovim): preview tag at cursor

--- config/nvim/after/ftplugin/help.lua
+++ config/nvim/after/ftplugin/help.lua
@@ -1,3 +1,4 @@
+local autocmd = dm.autocmd
 local nnoremap = dm.nnoremap
 
 vim.cmd [[
@@ -11,3 +12,19 @@ local opts = { buffer = true, nowait = true }
 nnoremap("q", "<Cmd>quit<CR>", opts)
 nnoremap("<CR>", "<C-]>", opts)
 nnoremap("<BS>", "<C-T>", opts)
+
+nnoremap("p", function()
+  vim.cmd "wincmd }"
+  -- Do *not* use the autocmd pattern `<buffer>` {{{
+  --
+  -- The preview window wouldn't be closed when we press `<Enter>` on a tag,
+  -- because – if the tag is defined in another file – `CursorMoved` would be
+  -- fired in the new buffer.
+  -- }}}
+  autocmd {
+    events = "CursorMoved",
+    targets = "*",
+    modifiers = "++once",
+    command = "pclose",
+  }
+end, opts)
