9e9605610be44fcca1581ec458390fce299c8234
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-08-21 18:36:55 +0530

    feat: enable autoreload in ipython repl

--- python/ipython/ipython_config.py
+++ python/ipython/ipython_config.py
@@ -32,11 +32,11 @@
 
 ## lines of code to run at IPython startup.
 #  Default: []
-# c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
 
 ## A list of dotted module names of IPython extensions to load.
 #  Default: []
-# c.InteractiveShellApp.extensions = []
+c.InteractiveShellApp.extensions = ["autoreload"]
 
 ## DEPRECATED. Dotted module name of a single extra IPython extension to load.
 #
