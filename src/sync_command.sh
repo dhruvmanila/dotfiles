ff3b3154cef6bec3d04f1b61ee78838763e9820b
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-08-31 16:36:10 +0530

    chore: remove `instant-markdown-d` from sync command
    as that is a local package

--- src/sync_command.sh
+++ src/sync_command.sh
@@ -16,7 +16,7 @@ fi
 if [[ $sync_all || $sync_python ]]; then
   header "Syncing requirements.txt with the global Python packages..."
   pipx list --json \
-    | jq -r '.venvs | keys | join("\n")' \
+    | jq --raw-output '.venvs | keys | join("\n")' \
     | tee "${PYTHON_GLOBAL_REQUIREMENTS}" \
     | xargs -I{} echo "    {}"
 fi
@@ -24,7 +24,7 @@ fi
 if [[ $sync_all || $sync_node ]]; then
   header "Syncing node_modules.txt with the global Node packages..."
   npm --global --json list \
-    | jq -r '.dependencies | keys | join("\n")' \
+    | jq --raw-output '.dependencies | del(."instant-markdown-d") | keys | join("\n")' \
     | tee "${NPM_GLOBAL_PACKAGES}" \
     | xargs -I{} echo "    {}"
 fi
