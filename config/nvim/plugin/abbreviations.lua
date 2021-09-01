d5c668d0cc800ae94025e9d9a6a167c7bf2696a6
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-09-01 16:50:48 +0530

    refactor: port abbreviations to lua

---
+++ config/nvim/plugin/abbreviations.lua
@@ -0,0 +1,36 @@
+local cmd = vim.cmd
+
+-- Helper function to create a *command* abbreviation.
+---@param short string
+---@param long string
+local function cabbrev(short, long)
+  cmd(
+    (
+      "cnoreabbrev <expr> %s getcmdtype() == ':' && getcmdpos() == %d ? '%s' : '%s'"
+    ):format(short, #short + 1, long, short)
+  )
+end
+
+-- Packer commands (`wbthomason/packer.nvim`)
+cabbrev("pc", "PackerClean")
+cabbrev("po", "PackerCompile")
+cabbrev("pi", "PackerInstall")
+cabbrev("ps", "PackerSync")
+cabbrev("pu", "PackerUpdate")
+
+-- Session commands
+cabbrev("sc", "SClose")
+cabbrev("sd", "SDelete")
+cabbrev("sl", "SLoad")
+cabbrev("sr", "SRename")
+cabbrev("ss", "SSave")
+
+-- :so -> :source %
+cabbrev("so", "source %")
+
+-- For better readability (`tpope/vim-scriptease`)
+cabbrev("mes", "Message")
+cabbrev("veb", "Verbose")
+
+cmd "inoreabbrev ie i.e.,"
+cmd "inoreabbrev eg e.g.,"
