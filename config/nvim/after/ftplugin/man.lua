6ee9b9087be9acb66dbc0c30c07575a1ba832b5d
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-09-09 21:26:10 +0530

    refactor(nvim): no tabline if used a MANPAGER

--- config/nvim/after/ftplugin/man.lua
+++ config/nvim/after/ftplugin/man.lua
@@ -5,3 +5,8 @@ local opts = { buffer = true, nowait = true }
 nnoremap("q", "<Cmd>quit<CR>", opts)
 nnoremap("<CR>", "<C-]>", opts)
 nnoremap("<BS>", "<C-T>", opts)
+
+-- Do not show the tabline if Neovim was opened as a MANPAGER.
+if #vim.api.nvim_list_wins() == 1 then
+  vim.o.showtabline = 0
+end
