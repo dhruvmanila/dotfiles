7fcbe5500172c2d27ea42291bc252835c74814f7
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-09-12 00:14:48 +0530

    fix(devicons): use new API to set custom icons
    https://github.com/kyazdani42/nvim-web-devicons/commit/1ae1f6ee8c06ef7ae7d20548af9fd84453184fb4

--- config/nvim/after/plugin/nvim_web_devicons.lua
+++ config/nvim/after/plugin/nvim_web_devicons.lua
@@ -1,20 +1,28 @@
-require("nvim-web-devicons").setup {
-  override = {
-    ["TelescopePrompt"] = {
-      icon = "",
-      color = "#f38019",
-      name = "TelescopePrompt",
-    },
-    ["Dashboard"] = {
-      icon = "",
-      color = "#787878",
-      name = "Dashboard",
-    },
-    ["[packer]"] = {
-      icon = "",
-      color = "#787878",
-      name = "Packer",
-    },
+local nvim_web_devicons = require "nvim-web-devicons"
+
+local custom_icons = {
+  ["TelescopePrompt"] = {
+    icon = "",
+    color = "#f38019",
+    name = "TelescopePrompt",
   },
-  default = true,
+  ["Dashboard"] = {
+    icon = "",
+    color = "#787878",
+    name = "Dashboard",
+  },
+  ["[packer]"] = {
+    icon = "",
+    color = "#787878",
+    name = "Packer",
+  },
 }
+
+if not nvim_web_devicons.has_loaded() then
+  nvim_web_devicons.setup {
+    overrides = custom_icons,
+    default = true,
+  }
+else
+  nvim_web_devicons.set_icon(custom_icons)
+end
