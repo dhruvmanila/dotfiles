1d79cecba01546b3d052825d6aeca68429801fab
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-09-16 01:16:29 +0530

    feat(sh): add completion for ShellCheckWiki command

--- config/nvim/after/ftplugin/sh.lua
+++ config/nvim/after/ftplugin/sh.lua
@@ -1,3 +1,13 @@
+-- Return a list of diagnostic codes of the current buffer for shellcheck.
+---@return string[]
+function _G.buf_shellcheck_error_codes()
+  local errorcodes = {}
+  for _, diagnostic in ipairs(vim.lsp.diagnostic.get()) do
+    table.insert(errorcodes, tostring(diagnostic.code))
+  end
+  return errorcodes
+end
+
 dm.command("ShellCheckWiki", function(errorcode)
   vim.fn["external#browser"](
     "https://github.com/koalaman/shellcheck/wiki/SC" .. errorcode
@@ -5,4 +15,5 @@ dm.command("ShellCheckWiki", function(errorcode)
 end, {
   buffer = true,
   nargs = 1,
+  complete = "customlist,v:lua.buf_shellcheck_error_codes",
 })
