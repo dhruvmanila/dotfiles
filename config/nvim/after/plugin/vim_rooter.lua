3fffed796eb3112bd2f45ff5e7ebc0a69177f6bd
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-01 17:47:15 +0530

    refactor(nvim): use rooter in manual mode

--- config/nvim/after/plugin/vim_rooter.lua
+++ config/nvim/after/plugin/vim_rooter.lua
@@ -1,4 +1,17 @@
-vim.g.rooter_cd_cmd = "lcd"
-vim.g.rooter_silent_chdir = 1
-vim.g.rooter_resolve_links = 1
-vim.g.rooter_patterns = { ".git" }
+-- Use `:RooterToggle` to toggle between automatic and manual behaviour.
+-- Use `:Rooter` to invoke Rooter manually.
+
+local g = vim.g
+
+-- Prefer using manual mode.
+g.rooter_manual_only = 1
+
+-- Only set the current directory for the current window.
+g.rooter_cd_cmd = "lcd"
+
+-- These are checked breadth-first as Rooter walks up the directory tree and the
+-- first match is used.
+g.rooter_patterns = { ".git", "requirements.txt" }
+
+g.rooter_silent_chdir = 1
+g.rooter_resolve_links = 1
