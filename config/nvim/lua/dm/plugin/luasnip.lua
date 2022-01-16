0979554fcb0d7b23b735213091216727e0f1fde9
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-16 09:03:36 +0530

    feat(nvim): snippets for go/python

--- config/nvim/lua/dm/plugin/luasnip.lua
+++ config/nvim/lua/dm/plugin/luasnip.lua
@@ -5,7 +5,6 @@ local t = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
 local f = ls.function_node
-local d = ls.dynamic_node
 
 -- Wrapper function around LuaSnip parse function which accepts table as the
 -- `body` argument, and will concatenate it before passing to the original function.
@@ -25,16 +24,48 @@ ls.config.set_config {
   updateevents = "TextChanged,TextChangedI",
 }
 
-ls.snippets.all = {
-  s(
-    { trig = "date" },
-    { f(function()
+ls.snippets.all = { -- {{{1
+  s({ trig = "todo", dscr = "Insert TODO comment with username" }, {
+    f(function()
+      return vim.bo.commentstring:gsub("%%s", "TODO(dhruvmanila): ")
+    end, {}),
+  }),
+
+  s({ trig = "date", dscr = "today's date in YYYY-mm-dd format" }, {
+    f(function()
       return { os.date "%Y-%m-%d" }
-    end, {}), i(0) }
-  ),
+    end, {}),
+    i(0),
+  }),
 }
 
-ls.snippets.lua = {
+ls.snippets.go = { -- {{{1
+  s({ trig = "main", dscr = "main function" }, {
+    t { "func main() {", "\t" },
+    i(0),
+    t { "", "}" },
+  }),
+
+  -- #1:
+  --     if err != nil {
+  --         return nil, err
+  --     }
+  --
+  -- #2:
+  --     if err != nil {
+  --         return err
+  --     }
+  s({ trig = "iferr", dscr = "if err is not nil block" }, {
+    t { "if err != nil {", "\treturn " },
+    c(1, {
+      sn(nil, { i(1, "nil"), t ", err" }),
+      t "err",
+    }),
+    t { "", "}" },
+  }),
+}
+
+ls.snippets.lua = { -- {{{1
   parse(
     { trig = "fmt", dscr = "Format string" },
     'string.format("${1:formatstring}", $2)$0'
@@ -50,15 +81,16 @@ ls.snippets.lua = {
     'local ${1:var} = require("${2:modname: string}")'
   ),
 
-  parse({ trig = "augroup", dscr = "Define augroup using native function" }, {
-    'dm.augroup("${1:name: string}", {',
-    "\t$2",
-    "})",
-  }),
-
-  s({ trig = "stylua" }, {
+  s({ trig = "ignore" }, {
     t "-- stylua: ignore",
     c(1, { t "", t " start", t " end" }),
     i(0),
+  }),
+}
+
+ls.snippets.python = { -- {{{1
+  s({ trig = "ifmain", dscr = 'if __name__ == "__main__":' }, {
+    t { 'if __name__ == "__main__":', "\t" },
+    i(1, "pass"),
   }),
 }
