a0d54fae139be16a1b8093ddae61356e182dbc44
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-24 00:37:43 +0530

    refactor: use the new on_attach callback for gitsigns
    Ref: https://github.com/lewis6991/gitsigns.nvim/commit/58e5d6d85e429bfc78fc425dff6d4053ed41753f

--- config/nvim/lua/dm/plugin/gitsigns.lua
+++ config/nvim/lua/dm/plugin/gitsigns.lua
@@ -1,4 +1,34 @@
-require("gitsigns").setup {
+local gitsigns = require "gitsigns"
+
+-- `on_attach` callback to setup buffer mappings for Gitsigns.
+---@param bufnr number
+local function on_attach(bufnr)
+  local function map(modes, lhs, rhs, opts)
+    opts = opts or {}
+    opts.buffer = bufnr
+    vim.keymap.set(modes, lhs, rhs, opts)
+  end
+
+  -- Navigation
+  map("n", "]c", "&diff ? ']c' : '<Cmd>Gitsigns next_hunk<CR>'", {
+    buffer = bufnr,
+  })
+  map("n", "[c", "&diff ? '[c' : '<Cmd>Gitsigns prev_hunk<CR>'", {
+    buffer = bufnr,
+  })
+
+  -- Actions
+  map({ "n", "v" }, "<leader>hs", gitsigns.stage_hunk)
+  map({ "n", "v" }, "<leader>hr", gitsigns.reset_hunk)
+  map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+  map("n", "<leader>hR", gitsigns.reset_buffer)
+  map("n", "<leader>hp", gitsigns.preview_hunk)
+
+  -- Text object
+  map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+end
+
+gitsigns.setup {
   signs = {
     add = { hl = "GitSignsAdd", text = "┃" },
     change = { hl = "GitSignsChange", text = "┃" },
@@ -14,4 +44,5 @@ require("gitsigns").setup {
     col = 1,
   },
   attach_to_untracked = false,
+  on_attach = on_attach,
 }
