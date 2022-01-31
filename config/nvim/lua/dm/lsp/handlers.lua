9fee0c0e6170880d45fd0e70859fd4149b26940f
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-31 09:18:45 +0530

    refactor(nvim/lsp): update location handler

--- config/nvim/lua/dm/lsp/handlers.lua
+++ config/nvim/lua/dm/lsp/handlers.lua
@@ -1,29 +1,34 @@
-local lsp = vim.lsp
-local api = vim.api
-local handlers = lsp.handlers
-
 -- Modified version of the original handler. This will open the quickfix
 -- window only if the response is a list and the count is greater than 1.
-local function location_handler(_, result, ctx)
-  if not result or vim.tbl_isempty(result) then
-    dm.notify("LSP (" .. ctx.method .. ")", "No results found")
+local function location_handler(err, result, ctx)
+  local title = "LSP (" .. ctx.method .. ")"
+  if err then
+    dm.notify(title, tostring(err))
+    return
+  end
+  if result == nil or vim.tbl_isempty(result) then
+    dm.notify(title, "No results found")
     return
   end
 
   -- Response: Location | Location[] | LocationLink[] | null
   -- https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_definition
   if vim.tbl_islist(result) then
+    vim.lsp.util.jump_to_location(result[1])
     if vim.tbl_count(result) > 1 then
-      vim.diagnostic.setqflist { title = ctx.method }
-      api.nvim_command "wincmd p"
+      vim.fn.setqflist(
+        vim.lsp.util.locations_to_items(result),
+        nil,
+        { title = title }
+      )
+      vim.api.nvim_command "wincmd p"
     end
-    lsp.util.jump_to_location(result[1])
   else
-    lsp.util.jump_to_location(result)
+    vim.lsp.util.jump_to_location(result)
   end
 end
 
-handlers["textDocument/definition"] = location_handler
-handlers["textDocument/declaration"] = location_handler
-handlers["textDocument/typeDefinition"] = location_handler
-handlers["textDocument/implementation"] = location_handler
+vim.lsp.handlers["textDocument/definition"] = location_handler
+vim.lsp.handlers["textDocument/declaration"] = location_handler
+vim.lsp.handlers["textDocument/typeDefinition"] = location_handler
+vim.lsp.handlers["textDocument/implementation"] = location_handler
