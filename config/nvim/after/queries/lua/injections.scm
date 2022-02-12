265da2ea71049897ba982c522c05bebdad54b2a8
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-02-12 16:33:46 +0530

    feat(lua): highlight query for `set_query` function

---
+++ config/nvim/after/queries/lua/injections.scm
@@ -0,0 +1,12 @@
+;; Highlight the query string in `vim.treesitter.set_query`
+((function_call
+  name: [
+    ;; `vim.treesitter.set_query` or `treesitter.set_query`
+    (dot_index_expression
+      field: (_) @_function_name)
+    ;; `set_query`
+    (_) @_function_name
+  ]
+  ;; Third argument is the query
+  arguments: (arguments (_) (_) (string content: _ @query))
+  (#eq? @_function_name "set_query")))
