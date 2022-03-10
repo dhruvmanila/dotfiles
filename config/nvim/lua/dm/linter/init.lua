61d445870a606712a2c6884013154c51bb840540
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-03-10 09:35:32 +0530

    feat(linter): actionlint for GitHub workflow files
    Ref: https://github.com/rhysd/actionlint

--- config/nvim/lua/dm/linter/init.lua
+++ config/nvim/lua/dm/linter/init.lua
@@ -96,9 +96,7 @@ do
 
   register("sh", {
     cmd = "shellcheck",
-    args = function()
-      return { "--format", "json", "-" }
-    end,
+    args = { "--format", "json", "-" },
     ignore_exitcode = true,
     parser = function(output)
       local diagnostics = {}
@@ -149,5 +147,27 @@ do
     end,
   })
 end
+
+register("yaml", {
+  cmd = "actionlint",
+  args = { "-no-color", "-format", "{{json .}}", "-" },
+  ignore_exitcode = true,
+  parser = function(output)
+    local diagnostics = {}
+    for _, item in ipairs(vim.json.decode(output)) do
+      table.insert(diagnostics, {
+        source = "actionlint",
+        lnum = item.line - 1,
+        end_lnum = item.line,
+        col = item.column - 1,
+        end_col = item.column,
+        severity = vim.diagnostic.severity.ERROR,
+        code = item.kind,
+        message = item.message,
+      })
+    end
+    return diagnostics
+  end,
+})
 
 return { lint = lint.lint }
