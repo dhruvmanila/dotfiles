2b35ecef6746ee388ce55ceeb6957e646ab3e0de
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-03-19 09:25:12 +0530

    fix: specify yaml schema explicitly for GitHub workflows

--- config/nvim/lua/dm/lsp/servers.lua
+++ config/nvim/lua/dm/lsp/servers.lua
@@ -114,5 +114,15 @@ return {
   -- https://github.com/redhat-developer/yaml-language-server
   -- Install: `npm install --global yaml-language-server`
   -- Settings: https://github.com/redhat-developer/yaml-language-server#language-server-settings
-  yamlls = {},
+  yamlls = {
+    settings = {
+      yaml = {
+        schemas = {
+          -- Specify this explicitly as the server gets confused with `hammerkit.json`
+          ---@see https://github.com/redhat-developer/vscode-yaml/issues/565
+          ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
+        },
+      },
+    },
+  },
 }
