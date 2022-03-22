ee5c72ccecd445bbb0797d5e0d0d41a4eb6608c7
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-03-22 10:46:22 +0530

    chore: simplify gitsigns keymap config
    No wrapper functions and remove description when not necessary.

--- config/nvim/lua/dm/plugins/gitsigns.lua
+++ config/nvim/lua/dm/plugins/gitsigns.lua
@@ -3,42 +3,41 @@ local gitsigns = require 'gitsigns'
 -- `on_attach` callback to setup buffer mappings for Gitsigns.
 ---@param bufnr number
 local function on_attach(bufnr)
-  local function map(modes, lhs, rhs, opts)
-    opts = opts or {}
-    opts.buffer = bufnr
-    vim.keymap.set(modes, lhs, rhs, opts)
-  end
-
   -- Navigation
-  map('n', ']c', "&diff ? ']c' : '<Cmd>Gitsigns next_hunk<CR>'", {
+  vim.keymap.set('n', ']c', "&diff ? ']c' : '<Cmd>Gitsigns next_hunk<CR>'", {
     expr = true,
-    desc = 'Gitsigns: Goto next hunk',
+    buffer = bufnr,
   })
-  map('n', '[c', "&diff ? '[c' : '<Cmd>Gitsigns prev_hunk<CR>'", {
+  vim.keymap.set('n', '[c', "&diff ? '[c' : '<Cmd>Gitsigns prev_hunk<CR>'", {
     expr = true,
-    desc = 'Gitsigns: Goto prev hunk',
+    buffer = bufnr,
   })
 
   -- Actions
-  map({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk, {
+  vim.keymap.set({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk, {
+    buffer = bufnr,
     desc = 'Gitsigns: Stage hunk',
   })
-  map({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk, {
+  vim.keymap.set({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk, {
+    buffer = bufnr,
     desc = 'Gitsigns: Reset hunk',
   })
-  map('n', '<leader>hu', gitsigns.undo_stage_hunk, {
+  vim.keymap.set('n', '<leader>hu', gitsigns.undo_stage_hunk, {
+    buffer = bufnr,
     desc = 'Gitsigns: Undo stage hunk',
   })
-  map('n', '<leader>hR', gitsigns.reset_buffer, {
+  vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer, {
+    buffer = bufnr,
     desc = 'Gitsigns: Reset buffer',
   })
-  map('n', '<leader>hp', gitsigns.preview_hunk, {
+  vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, {
+    buffer = bufnr,
     desc = 'Gitsigns: Preview hunk',
   })
 
   -- Text object
-  map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', {
-    desc = 'Gitsigns: Select hunk (text object)',
+  vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', {
+    buffer = bufnr,
   })
 end
 
