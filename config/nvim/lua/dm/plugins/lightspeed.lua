cdc934ee92570fda6805ab3753fd3d967468b7b4
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-03-23 19:01:56 +0530

    refactor: use setup function instead

--- config/nvim/lua/dm/plugins/lightspeed.lua
+++ config/nvim/lua/dm/plugins/lightspeed.lua
@@ -2,17 +2,15 @@
 -- in the future, if I like the `s`/`S` behavior, I might enable it, but most
 -- likely remap it to a different key like `<leader>j`/`<leader>k`.
 
--- Do NOT set any mappings by default.
+-- This should be set before setting up the plugin.
 vim.g.lightspeed_no_default_keymaps = true
 
--- Setting lightspeed options via the `opts` table directly
-local opts = require('lightspeed').opts
-
--- For 1-character search, only the next 'n' matches will be highlighted.
-opts.limit_ft_matches = 10
-
--- Timeout value (ms) after which the plugin should exit f/t-mode.
-opts.exit_after_idle_msecs.unlabeled = 2000
+require('lightspeed').setup {
+  limit_ft_matches = 10,
+  exit_after_idle_msecs = {
+    unlabeled = 2000,
+  },
+}
 
 vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>Lightspeed_f')
 vim.keymap.set({ 'n', 'x', 'o' }, 'F', '<Plug>Lightspeed_F')
