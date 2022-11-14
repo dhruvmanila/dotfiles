ff080d4f75b860531ca17b0f9fde5f1289a00359
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-11-14 19:32:53 +0530

    feat(linter): golangci-lint

--- config/nvim/lua/dm/linter/init.lua
+++ config/nvim/lua/dm/linter/init.lua
@@ -34,6 +34,87 @@ do
   })
 end
 
+-- go:golangci-lint {{{1
+
+do
+  local config_path
+
+  local severity_map = {
+    error = vim.diagnostic.severity.ERROR,
+    warning = vim.diagnostic.severity.WARN,
+    refactor = vim.diagnostic.severity.INFO,
+    convention = vim.diagnostic.severity.HINT,
+  }
+
+  register('go', {
+    cmd = 'golangci-lint',
+    args = function(bufnr)
+      return {
+        'run',
+        '--out-format',
+        'json',
+        '--config',
+        config_path,
+        -- Path to the package directory.
+        vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)),
+      }
+    end,
+    -- Enable the linter only if there's a config file in the project.
+    enable = function(bufnr)
+      config_path = vim.fs.find({
+        -- https://golangci-lint.run/usage/configuration/#config-file
+        '.golangci.yml',
+        '.golangci.yaml',
+        '.golangci.toml',
+        '.golangci.json',
+      }, {
+        path = vim.api.nvim_buf_get_name(bufnr),
+        upward = true,
+        type = 'file',
+      })[1]
+      return config_path ~= nil
+    end,
+    stdin = false,
+    append_fname = false,
+    ignore_exitcode = true,
+    parser = function(output, bufnr)
+      if output == '' then
+        return {}
+      end
+
+      local decoded = vim.json.decode(output)
+      if decoded.Issues == nil then
+        return {}
+      end
+
+      local diagnostics = {}
+      local cwd = vim.loop.cwd()
+      local current_file = vim.api.nvim_buf_get_name(bufnr)
+
+      for _, item in ipairs(decoded.Issues) do
+        local linted_file = cwd .. '/' .. item.Pos.Filename
+        -- Publish diagnostics only for the current file.
+        if linted_file == current_file then
+          -- Diagnostic uses 0-based rows and columns.
+          ---@see |diagnostic-structure|
+          local lnum = item.Pos.Line > 0 and item.Pos.Line - 1 or 0
+          local col = item.Pos.Column > 0 and item.Pos.Column - 1 or 0
+          table.insert(diagnostics, {
+            lnum = lnum,
+            end_lnum = lnum,
+            col = col,
+            end_col = col,
+            severity = severity_map[item.Severity] or severity_map.warning,
+            source = item.FromLinter,
+            message = item.Text,
+          })
+        end
+      end
+      return diagnostics
+    end,
+  })
+end
+
 -- python:flake8 {{{1
 do
   local pat = '[^:]+:(%d+):(%d+):(([EWF])%w+):(.+)'
