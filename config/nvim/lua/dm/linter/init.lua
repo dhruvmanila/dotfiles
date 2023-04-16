7599eba1547cb7eae617b783fc78382213474d6e
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-04-16 18:34:28 +0530

    Remove `ruff` linter (using `ruff-lsp`)

--- config/nvim/lua/dm/linter/init.lua
+++ config/nvim/lua/dm/linter/init.lua
@@ -205,34 +205,6 @@ do
   })
 end
 
--- python:ruff {{{1
-
-register('python', {
-  cmd = 'ruff',
-  args = { 'check', '--exit-zero', '--format', 'json' },
-  stdin = false,
-  parser = function(output)
-    local diagnostics = {}
-    for _, item in ipairs(vim.json.decode(output)) do
-      -- Ignore all the fixed diagnostics. This will be true if a fix was
-      -- available and the `--fix` flag was passed.
-      if not item.fixed then
-        table.insert(diagnostics, {
-          source = 'ruff',
-          lnum = math.max(item.location.row - 1, 0),
-          end_lnum = math.max(item.end_location.row - 1, 0),
-          col = math.max(item.location.column - 1, 0),
-          end_col = math.max(item.end_location.column - 1, 0),
-          severity = vim.diagnostic.severity.WARN,
-          code = item.code,
-          message = item.message,
-        })
-      end
-    end
-    return diagnostics
-  end,
-})
-
 -- yaml:actionlint {{{1
 register('yaml', {
   cmd = 'actionlint',
