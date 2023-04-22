2f4d4205b9db73ba038446d7aae99dcb881b62e3
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-04-23 02:46:12 +0530

    Add `sqlfluff` for linting `sql` files

--- config/nvim/lua/dm/linter/init.lua
+++ config/nvim/lua/dm/linter/init.lua
@@ -205,6 +205,39 @@ do
   })
 end
 
+-- sql:sqlfluff {{{1
+
+register('sql', {
+  cmd = 'sqlfluff',
+  args = {
+    'lint',
+    '--disable-progress-bar',
+    '--nocolor',
+    '--nofail',
+    '--format=json',
+    '--dialect=postgres',
+  },
+  stdin = false,
+  parser = function(output)
+    local diagnostics = {}
+    for _, item in ipairs(vim.json.decode(output)) do
+      for _, violation in ipairs(item.violations) do
+        table.insert(diagnostics, {
+          source = 'sqlfluff',
+          lnum = violation.line_no - 1,
+          end_lnum = violation.line_no,
+          col = violation.line_pos - 1,
+          end_col = violation.line_pos,
+          severity = vim.diagnostic.severity.WARN,
+          code = violation.code,
+          message = violation.description,
+        })
+      end
+    end
+    return diagnostics
+  end,
+})
+
 -- yaml:actionlint {{{1
 register('yaml', {
   cmd = 'actionlint',
