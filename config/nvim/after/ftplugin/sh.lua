c43770c348ed9f8323ca1bf23e123dd1821a68bf
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-05-08 17:47:26 +0530

    fix: shellcheck diagnostic codes come from lsp

--- config/nvim/after/ftplugin/sh.lua
+++ config/nvim/after/ftplugin/sh.lua
@@ -1,19 +1,33 @@
----@type number
-local namespace = vim.api.nvim_get_namespaces()['dm__diagnostics_sh_shellcheck']
+-- Language server name for shell files.
+local server_name = 'bashls'
+
+---@type number?
+local namespace
 
 -- Return a list of diagnostic codes of the current buffer for shellcheck.
 ---@return string[]
 local function buf_shellcheck_error_codes()
+  if namespace == nil then
+    local client_id = vim.lsp.get_active_clients({
+      bufnr = vim.api.nvim_get_current_buf(),
+      name = server_name,
+    })[1].id
+    namespace = vim.api.nvim_get_namespaces()[('vim.lsp.%s.%d'):format(
+      server_name,
+      client_id
+    )]
+  end
   local errorcodes = {}
   for _, diagnostic in ipairs(vim.diagnostic.get(0, { namespace = namespace })) do
-    table.insert(errorcodes, tostring(diagnostic.code))
+    -- This is to avoid duplicates.
+    errorcodes[tostring(diagnostic.code)] = true
   end
-  return errorcodes
+  return vim.tbl_keys(errorcodes)
 end
 
 vim.api.nvim_buf_create_user_command(0, 'ShellCheckWiki', function(opts)
   vim.fn['external#browser'](
-    'https://github.com/koalaman/shellcheck/wiki/SC' .. opts.args
+    'https://github.com/koalaman/shellcheck/wiki/' .. opts.args
   )
 end, {
   nargs = 1,
