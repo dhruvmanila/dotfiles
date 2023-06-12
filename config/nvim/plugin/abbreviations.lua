9cf3a4ed67fcecc37a43dca465dd9a822c05c99f
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-06-12 12:57:16 +0530

    refactor: use `nvim_set_keymap` for abbreviations

--- config/nvim/plugin/abbreviations.lua
+++ config/nvim/plugin/abbreviations.lua
@@ -1,17 +1,24 @@
-local cmd = vim.cmd
-
--- Helper function to create a *command* abbreviation.
+-- Helper function to create an abbreviation for normal Ex command at the
+-- start position.
+--
+-- For example, if short is 'nm' and long is 'Neovim', then:
+--    :nm -> :Neovim
+--    /nm -> /nm  (not an Ex command)
+--    :foo nm -> :foo nm (not at the start position)
 ---@param short string
 ---@param long string
 local function cabbrev(short, long)
-  cmd(
-    ("cnoreabbrev <expr> %s getcmdtype() == ':' && getcmdpos() == %d ? '%s' : '%s'"):format(
-      short,
-      #short + 1,
-      long,
-      short
-    )
-  )
+  local cmdpos = #short + 1
+  vim.api.nvim_set_keymap('ca', short, '', {
+    expr = true,
+    callback = function()
+      if vim.fn.getcmdtype() == ':' and vim.fn.getcmdpos() == cmdpos then
+        return long
+      else
+        return short
+      end
+    end,
+  })
 end
 
 -- Session commands
