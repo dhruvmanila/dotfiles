4c2f07ab0b5dcaa51b8391d8b64ed35b99226b63
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-06-25 21:22:39 +0530

    chore: update minimal.lua for lazy.nvim

--- config/nvim/minimal.lua
+++ config/nvim/minimal.lua
@@ -1,49 +1,34 @@
--- Load the 'runtime/' files
-vim.cmd [[set runtimepath=$VIMRUNTIME]]
+-- DO NOT change the paths and don't remove the colorscheme
+local root = vim.fn.fnamemodify('./.repro', ':p')
 
--- Originally, `packpath` contains a lot of path to search into which also
--- includes the `~/.config/nvim` directory. Now, if we open Neovim, the files
--- in the `plugin/`, `ftplugin/`, etc. directories will be loaded automatically.
---
--- We will set the value of `packpath` to contain only our testing directory to
--- avoid loading files from our config directory.
---
---     $ nvim -nu minimal.lua
-vim.cmd [[set packpath=/tmp/nvim/site]]
-
-local package_root = '/tmp/nvim/site/pack'
-local packer_install_path = package_root .. '/packer/start/packer.nvim'
-
-local function load_plugins()
-  require('packer').startup {
-    {
-      'wbthomason/packer.nvim',
-      -- Add plugins to test...
-    },
-    config = {
-      package_root = package_root,
-      compile_path = packer_install_path .. '/plugin/packer_compiled.lua',
-      display = { non_interactive = true },
-    },
-  }
+-- set stdpaths to use .repro
+for _, name in ipairs { 'config', 'data', 'state', 'cache' } do
+  vim.env[('XDG_%s_HOME'):format(name:upper())] = root .. '/' .. name
 end
 
-_G.load_config = function()
-  -- Add the necessary `init.lua` settings which could include the setup
-  -- functions for the plugins...
-end
-
-if vim.fn.isdirectory(packer_install_path) == 0 then
-  print 'Installing plugins and dependencies...'
+-- bootstrap lazy
+local lazypath = root .. '/plugins/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
     'git',
     'clone',
-    '--depth=1',
-    'https://github.com/wbthomason/packer.nvim',
-    packer_install_path,
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    lazypath,
   }
 end
+vim.opt.runtimepath:prepend(lazypath)
 
-load_plugins()
-require('packer').sync()
-vim.cmd [[autocmd User PackerComplete ++once echo "Ready!" | lua load_config()]]
+-- install plugins
+local plugins = {
+  'folke/tokyonight.nvim',
+  -- add any other plugins here
+}
+
+require('lazy').setup(plugins, {
+  root = root .. '/plugins',
+})
+
+vim.cmd.colorscheme 'tokyonight'
+
+-- add anything else here
