5d14e9ad6ee7dfa3ee891c29ff2c0ae7f2fae436
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-07-29 21:43:50 +0530

    fix: create temp file for each kitty scrollback buffer

--- config/kitty/pager.sh
+++ config/kitty/pager.sh
@@ -5,7 +5,11 @@
 # Actual solution which doesn't work for me :(
 #   https://github.com/kovidgoyal/kitty/issues/719#issuecomment-952039731
 
+TMP_FILE="$(mktemp -t kitty_scrollback_buffer)"
+
 INPUT_LINE_NUMBER=${1:-0} CURSOR_LINE=${2:-1} CURSOR_COLUMN=${3:-1} ~/neovim/bin/nvim \
   --clean \
   -u ~/.config/kitty/init.lua \
-  -c "silent write! /tmp/kitty_scrollback_buffer | terminal cat /tmp/kitty_scrollback_buffer - "
+  -c "silent write! $TMP_FILE | terminal cat $TMP_FILE - "
+
+rm "$TMP_FILE"
