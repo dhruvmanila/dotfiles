d73cd24e3bb4acd99faa4d3699b638c8eecba70f
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-30 14:06:27 +0530

    nvim: remove markdown preview impl

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,3 @@
 -- vim.opt_local.textwidth = 80
 vim.opt_local.wrap = true
 vim.opt_local.conceallevel = 0
-
-vim.api.nvim_buf_create_user_command(0, 'Preview', require('dm.markdown').preview, { bar = true })
