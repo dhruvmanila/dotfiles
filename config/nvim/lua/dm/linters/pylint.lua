1763361fd79bc505b0aef399a4a541c00f50c64b
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-30 14:07:12 +0530

    nvim: config for pylint linter

---
+++ config/nvim/lua/dm/linters/pylint.lua
@@ -0,0 +1,36 @@
+local severity_map = {
+  error = vim.diagnostic.severity.ERROR,
+  fatal = vim.diagnostic.severity.ERROR,
+  warning = vim.diagnostic.severity.WARN,
+  refactor = vim.diagnostic.severity.INFO,
+  info = vim.diagnostic.severity.INFO,
+  convention = vim.diagnostic.severity.HINT,
+}
+
+return {
+  cmd = 'pylint',
+  args = { '--output-format', 'json' },
+  ignore_exitcode = true,
+  parser = function(output, bufnr)
+    local diagnostics = {}
+    local current_file = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ':.')
+
+    for _, item in ipairs(vim.json.decode(output)) do
+      if not item.path or item.path == current_file then
+        local column = item.column > 0 and item.column or 0
+        local end_column = item.endColumn ~= vim.NIL and item.endColumn or column
+        table.insert(diagnostics, {
+          source = 'pylint',
+          lnum = item.line - 1,
+          col = column,
+          end_lnum = item.line - 1,
+          end_col = end_column,
+          severity = assert(severity_map[item.type], 'missing mapping for severity ' .. item.type),
+          message = ('%s (%s)'):format(item.message, item.symbol),
+          code = item['message-id'],
+        })
+      end
+    end
+    return diagnostics
+  end,
+}
