bc34deffa158037c4fd29fe8bdaeec8c224b6914
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-30 14:12:36 +0530

    nvim: remove `vim-rooter`

--- config/nvim/lua/dm/plugins/init.lua
+++ config/nvim/lua/dm/plugins/init.lua
@@ -24,24 +24,6 @@ return {
   },
 
   {
-    'airblade/vim-rooter',
-    init = function()
-      -- Prefer using manual mode.
-      vim.g.rooter_manual_only = 1
-
-      -- Only set the current directory for the current window.
-      vim.g.rooter_cd_cmd = 'lcd'
-
-      -- These are checked breadth-first as Rooter walks up the directory tree and the
-      -- first match is used.
-      vim.g.rooter_patterns = { '.git', 'requirements.txt' }
-
-      vim.g.rooter_silent_chdir = 1
-      vim.g.rooter_resolve_links = 1
-    end,
-  },
-
-  {
     'junegunn/vim-easy-align',
     keys = {
       { 'ga', '<Plug>(EasyAlign)', mode = { 'n', 'x' } },
