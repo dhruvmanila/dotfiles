42954a78a76e913fce265a601a3a78230e6f10a2
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-08-03 10:13:12 +0530

    dark-mode-notifier: generic mac script
    The problem here is that the kitty executable path could be different
    depending on whether it's a M-series or intel Mac. I tried adding a
    which-like functionality but that didn't work. Then, I realize that
    the application path would still be the same and it contains the kitty
    executable so why not just use that instead.
    
    Also, let's avoid committing the executable, we'll create it on a new
    machine.

--- scripts/dark-mode-notifier/README.md
+++ scripts/dark-mode-notifier/README.md
@@ -4,7 +4,7 @@ A simple executable that listens for changes in the macOS dark mode setting and
 updates the Kitty terminal's theme accordingly.
 
 Run `make` from the root directory to build and install the executable. The
-executable will be installed to `/usr/local/bin/dark-mode-notifier`. It'll
+executable will be installed to `~/.local/bin/dark-mode-notifier`. It'll
 also be installed as a launch agent to run on login.
 
 Reference: https://github.com/mnewt/dotemacs/blob/8a153b133d289867415e99665d49e288cc0def5d/bin/dark-mode-notifier.swift
--- scripts/dark-mode-notifier/main.swift
+++ scripts/dark-mode-notifier/main.swift
@@ -2,29 +2,36 @@ import Cocoa
 
 @discardableResult
 func shell(_ cmd: String, args: String...) -> Int32 {
-    let task = Process()
-    task.launchPath = cmd
-    task.arguments = args
-    task.launch()
-    task.waitUntilExit()
-    return task.terminationStatus
+  let task = Process()
+  task.launchPath = cmd
+  task.arguments = args
+  task.launch()
+  task.waitUntilExit()
+  return task.terminationStatus
 }
 
 func updateKittyTheme() {
-    let kittyApps = NSRunningApplication.runningApplications(withBundleIdentifier: "net.kovidgoyal.kitty")
-    if kittyApps.count > 0 {
-        let isDark = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
-        print("Notifying Kitty that macOS Dark Mode is:", isDark)
-        shell("/usr/local/bin/kitty", args: "+kitten", "themes", "--reload-in=all", isDark ? "Gruvbox Dark" : "Gruvbox Light")
-    }
+  let kittyApps = NSRunningApplication.runningApplications(
+    withBundleIdentifier: "net.kovidgoyal.kitty")
+  if kittyApps.count > 0 {
+    let isDark = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
+    print("Notifying Kitty that macOS Dark Mode is:", isDark)
+
+    let kittyExecutable = "/Applications/kitty.app/Contents/MacOS/kitty"
+    print("Using the executable:", kittyExecutable)
+
+    shell(
+      kittyExecutable, args: "+kitten", "themes", "--reload-in=all",
+      isDark ? "Gruvbox Dark" : "Gruvbox Light")
+  }
 }
 
 updateKittyTheme()
 
 DistributedNotificationCenter.default.addObserver(
-    forName: Notification.Name("AppleInterfaceThemeChangedNotification"),
-    object: nil,
-    queue: nil
+  forName: Notification.Name("AppleInterfaceThemeChangedNotification"),
+  object: nil,
+  queue: nil
 ) { (notification) in updateKittyTheme() }
 
 // Enter cocoa run loop and start listening for notifyd events
