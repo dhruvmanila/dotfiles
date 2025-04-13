5af977ec9f08df485895e1296d131528a769481e
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-04-13 15:56:55 -0500

    nvim: remove find files in crates finder

--- config/nvim/lua/dm/projects.lua
+++ config/nvim/lua/dm/projects.lua
@@ -1,63 +1,7 @@
 local M = {}
 
+-- Setup for the ruff project.
 local function setup_ruff()
-  local action_state = require 'telescope.actions.state'
-  local actions = require 'telescope.actions'
-  local builtin = require 'telescope.builtin'
-  local finders = require 'telescope.finders'
-  local pickers = require 'telescope.pickers'
-  local telescope_config = require('telescope.config').values
-
-  vim.keymap.set('n', '<leader>fc', function()
-    local crate = vim.fs.root(0, 'Cargo.toml')
-    if crate == nil then
-      return
-    end
-    builtin.find_files {
-      prompt_title = ('Find Files (%s)'):format(vim.fs.basename(crate)),
-      cwd = crate,
-    }
-  end, { desc = 'telescope: find files in the current crate' })
-
-  vim.keymap.set('n', '<leader>fC', function()
-    local crates_dir = vim.fs.joinpath(dm.CWD, 'crates')
-
-    ---@type { path: string, name: string }[]
-    local crates = {}
-    for name, type in vim.fs.dir(crates_dir) do
-      if type == 'directory' then
-        table.insert(crates, { path = vim.fs.joinpath(crates_dir, name), name = name })
-      end
-    end
-
-    pickers
-      .new({}, {
-        prompt_title = 'Find in crates',
-        finder = finders.new_table {
-          results = crates,
-          entry_maker = function(entry)
-            return { display = entry.name, value = entry, ordinal = entry.name }
-          end,
-        },
-        previewer = false,
-        sorter = telescope_config.generic_sorter(),
-        attach_mappings = function()
-          actions.select_default:replace(function(prompt_bufnr)
-            local selection = action_state.get_selected_entry().value
-            actions.close(prompt_bufnr)
-            vim.schedule(function()
-              builtin.find_files {
-                prompt_title = ('Find in crate (%s)'):format(selection.name),
-                cwd = selection.path,
-              }
-            end)
-          end)
-          return true
-        end,
-      })
-      :find()
-  end, { desc = 'telescope: find files in the specific crate' })
-
   vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('dm__lsp_attach_ruff', { clear = true }),
     callback = function(args)
@@ -68,7 +12,7 @@ local function setup_ruff()
       if client.name == 'rust_analyzer' then
         -- Override the existing key binding to only include diagnostics from rust-analyzer.
         vim.keymap.set('n', '<leader>fd', function()
-          builtin.diagnostics {
+          require('telescope.builtin').diagnostics {
             prompt_title = 'Workspace Diagnostics (rust-analyzer)',
             namespace = vim.lsp.diagnostic.get_namespace(client.id),
           }
