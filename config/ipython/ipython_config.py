9b927e25adfbb7f67b5ae567205830fe2bd98814
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-06-07 18:03:58 +0530

    ipython: disable autoreload

--- config/ipython/ipython_config.py
+++ config/ipython/ipython_config.py
@@ -32,11 +32,11 @@
 
 ## lines of code to run at IPython startup.
 #  Default: []
-c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+# c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
 
 ## A list of dotted module names of IPython extensions to load.
 #  Default: []
-c.InteractiveShellApp.extensions = ["autoreload"]
+# c.InteractiveShellApp.extensions = ["autoreload"]
 
 ## DEPRECATED. Dotted module name of a single extra IPython extension to load.
 #
