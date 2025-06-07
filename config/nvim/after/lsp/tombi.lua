4344fb4f7b937ab1ce28aa5fb8bdcdb8274986f9
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-06-07 18:04:42 +0530

    nvim: use upstream tombi config

--- config/nvim/after/lsp/tombi.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/tombi-toml/tombi
--- Documentation: https://tombi-toml.github.io/tombi/docs
--- Install: `uv tool install tombi@latest`
----@type vim.lsp.Config
-return {
-  cmd = { 'tombi', 'serve' },
-  filetypes = { 'toml' },
-  single_file_support = true,
-}
