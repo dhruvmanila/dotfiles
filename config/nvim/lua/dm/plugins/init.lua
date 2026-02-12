219b8e18bde3a9fa3cb25c1da1b17dc702c4c970
Dhruv Manilawala <dhruvmanila@gmail.com>
2026-02-12 09:22:45 +0530

    nvim: remove deprecated flit.nvim, use leap.nvim

--- config/nvim/lua/dm/plugins/init.lua
+++ config/nvim/lua/dm/plugins/init.lua
@@ -15,10 +15,45 @@ return {
   { 'nacro90/numb.nvim', event = 'CmdlineEnter', config = true },
 
   {
-    'ggandor/flit.nvim',
+    'https://codeberg.org/andyg/leap.nvim',
     keys = { { 'f' }, { 'F' }, { 't' }, { 'T' } },
-    dependencies = { 'ggandor/leap.nvim' },
-    config = true,
+    config = function()
+      -- Ref: https://codeberg.org/andyg/leap.nvim#search-and-motions
+      local function as_ft(key_specific_args)
+        local common_args = {
+          inputlen = 1,
+          inclusive = true,
+          -- To limit search scope to the current line:
+          -- pattern = function (pat) return '\\%.l'..pat end,
+          opts = {
+            labels = '', -- force autojump
+            safe_labels = vim.fn.mode(1):match '[no]' and '' or nil, -- [1]
+          },
+        }
+        return vim.tbl_deep_extend('keep', common_args, key_specific_args)
+      end
+
+      local clever = require('leap.user').with_traversal_keys -- [2]
+      local clever_f = clever('f', 'F')
+      local clever_t = clever('t', 'T')
+
+      for key, key_specific_args in pairs {
+        f = { opts = clever_f },
+        F = { backward = true, opts = clever_f },
+        t = { offset = -1, opts = clever_t },
+        T = { backward = true, offset = 1, opts = clever_t },
+      } do
+        vim.keymap.set({ 'n', 'x', 'o' }, key, function()
+          require('leap').leap(as_ft(key_specific_args))
+        end)
+      end
+
+      -- [1] Match the modes here for which you don't want to use labels
+      --     (`:h mode()`, `:h lua-pattern`).
+      -- [2] This helper function makes it easier to set "clever-f"-like functionality
+      --     (https://github.com/rhysd/clever-f.vim), returning an `opts` table derived from the
+      --     defaults, where the given keys are added to `keys.next_target` and `keys.prev_target`
+    end,
   },
 
   {
