0b92126ff6a99cfcc5750676669c8c01f0c89133
Dhruv Manilawala <dhruvmanila@gmail.com>
2026-02-12 09:19:59 +0530

    nvim: disable codelens in lua lsp

--- config/nvim/after/lsp/lua_ls.lua
+++ config/nvim/after/lsp/lua_ls.lua
@@ -1,12 +1,12 @@
 -- https://github.com/LuaLS/lua-language-server
 -- Install: `brew install lua-language-server`
--- Settings: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+-- Settings: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
 ---@type vim.lsp.Config
 return {
   settings = {
     Lua = {
-      codelens = {
-        enable = true,
+      codeLens = {
+        enable = false,
       },
       completion = {
         -- Do NOT show contextual words, I got `cmp-buffer` for that.
