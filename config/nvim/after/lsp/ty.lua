5bf014296eff81d7b83be1b899e706d9c01763e3
Dhruv Manilawala <dhruvmanila@gmail.com>
2026-05-17 08:52:19 +0530

    nvim: simplify ty lsp config

--- config/nvim/after/lsp/ty.lua
+++ config/nvim/after/lsp/ty.lua
@@ -1,9 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { dm.OS_HOMEDIR .. '/work/astral/ruff/target/debug/ty', 'server' },
-  filetypes = { 'python' },
-  root_markers = { 'pyproject.toml', 'ty.toml' },
-  single_file_support = true,
+  cmd = { 'ty', 'server' },
   init_options = {
     logLevel = 'debug',
     logFile = vim.fn.stdpath 'log' .. '/lsp.ty.log',
