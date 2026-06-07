5f9a006af4914e283ca93e99e769cb2a66314965
Dhruv Manilawala <dhruvmanila@gmail.com>
2026-06-07 10:26:43 +0530

    kitty: use `nvim` from `PATH`

--- config/kitty/pager.sh
+++ config/kitty/pager.sh
@@ -4,7 +4,7 @@
 
 TMP_FILE="$(mktemp -t kitty_scrollback_buffer)"
 
-KITTY_SCROLLBACK=1 INPUT_LINE_NUMBER=${1:-0} CURSOR_LINE=${2:-1} CURSOR_COLUMN=${3:-1} ~/neovim/bin/nvim \
+KITTY_SCROLLBACK=1 INPUT_LINE_NUMBER=${1:-0} CURSOR_LINE=${2:-1} CURSOR_COLUMN=${3:-1} nvim \
   -c "silent write! $TMP_FILE | terminal cat $TMP_FILE -"
 
 rm "$TMP_FILE"
